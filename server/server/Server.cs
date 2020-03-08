01b3367d242d91af3d9b480e8ecc917448ac69a6
Kirill Poroh <procompteam@gmail.com>
2020-03-08 15:11:04 +0300

    #1 Server point

---
+++ server/server/Server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace server
+{
+	public static class Server
+	{
+		public const string BUILD_VERSION = "April 2019";
+		public const int COMPATIBILITY_CODE = 1;
+		
+		public static void Main()
+		{
+			
+		}
+	}
+}
