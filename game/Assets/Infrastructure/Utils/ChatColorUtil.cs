16cdc495bae7aa91de3bb8c75cba2c130a6f22c4
GGWP5 <assasin_creed061@mail.ru>
2020-03-14 14:31:38 +0300

    #1 Added util for working with chat color in future(not completed yet)

---
+++ game/Assets/Infrastructure/Utils/ChatColorUtil.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Drawing.Color;
+
+namespace game.Assets.Infrastructure.Utils
+{
+
+	public static class ChatColorUtil
+	{
+		
+		public static string UtilFunction1(R, G, B : integer)
+		{
+			
+		}
+		
+		public static UtilFunction2
+		{
+			
+		}
+		
+		public static UtilFunction3
+		{
+			
+		}
+		
+	}
+	
+}
