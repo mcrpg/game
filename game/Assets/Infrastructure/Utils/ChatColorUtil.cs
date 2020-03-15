b494664877bc0ff02e68f7614ac6ade3dd6d98a0
GGWP5 <assasin_creed061@mail.ru>
2020-03-15 16:41:51 +0300

    #5 Create static ChatColor util for work with chat color in future (Not completed yet)

--- game/Assets/Infrastructure/Utils/ChatColorUtil.cs
+++ game/Assets/Infrastructure/Utils/ChatColorUtil.cs
@@ -1,27 +1,29 @@
 using System;
-using System.Drawing.Color;
+using System.Drawing;
 
 namespace game.Assets.Infrastructure.Utils
 {
-
-	public static class ChatColorUtil
+	public class ChatColorUtil
 	{
-		
 		public static string UtilFunction1(R, G, B : integer)
 		{
-			
+			Color color = Color.FromArgb(R, G, B);
+
+			return new UtilFunction1( "["+color+"("+R+","+G+","+B")]" );
 		}
-		
-		public static UtilFunction2
+
+		public static string UtilFunction2(color : System.Drawing.Color)
 		{
+			// Вставьте сюда свой код,я не ебу что тут быть должно
 			
+			return new UtilFunction2( "["+color+"("+R+","+G+","+B")]" );
 		}
-		
-		public static UtilFunction3
+
+		public static string UtilFunction3("[color(R,G,B)]" : string)
 		{
-			
+			// И сюда
+
+			return System.Drawing.Color;
 		}
-		
 	}
-	
 }
